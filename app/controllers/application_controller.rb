ff58f47ef622d9744f13f6f48ff9aca51f374a3d
jose luis <cortezluisjose@gmail.com>
2020-05-14 00:12:58 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,49 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  get '/articles/new' do
+
+    erb :new
+  end
+
+  get '/articles' do
+    @article = Article.all
+
+    erb :index
+  end
+
+  post '/articles' do
+    @article = Article.create(params[:article])
+
+
+    erb :show
+  end
+
+  get '/articles/:id' do
+    @article = Articles.find(params[:id])
+
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(params[:edit_article])
+    @article.save
+
+    erb :show
+  end
+
+  delete '/articles/:id/delete' do
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    erb :delete
+  end
+
 end
